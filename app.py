8ec0bbd476426490ad067a3d4f9bed32916461be
Surendra Prajapat <97840357+Surendraprajapat18@users.noreply.github.com>
2024-03-31 19:35:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def predict_sentiment(input_text):
 
 
 def main():
-    st.markdown("# **<p align="center"><u>IoT-BASED SMART PARKING</u></p>**")
+    st.markdown("# **<p align="center"><u>NLP for Sentiment Analysis 📶 Prediction App</u></p>**")
     st.markdown("### By Surendra Prajapat")
 
     st.markdown("""#### Introduction:
